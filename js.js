8f651dd03a7f55031c9490889719353ded421ace
IsabelCMD <45418246+FlappyNarwal@users.noreply.github.com>
2020-05-22 13:05:14 +0200

    added all hover animations and linked javascript file

---
+++ js.js
@@ -0,0 +1,11 @@
+const blueCircle = document.getElementsByClassName('st2-3')[0];
+
+
+
+
+    
+    blueCircle.addEventListener('touchstart', function(){
+        console.log('hallo')
+        blueCircle.classList.toggle('big');
+    })
+
